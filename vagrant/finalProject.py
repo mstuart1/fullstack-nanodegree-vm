45f7e455281f8babb6c5da46892db454425d87d0
Michelle Stuart <michelle.stuart@rutgers.edu>
2019-12-02 14:04:08 -0500

    routing working

--- vagrant/finalProject.py
+++ vagrant/finalProject.py
@@ -4,40 +4,36 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/restaurants')
-def showRestaurants()
+def showRestaurants():
 	return "This page will show all of my restaurants"
 
 @app.route('/restaurant/new')
-def newRestaurant()
+def newRestaurant():
 	return "This page will be for making a new restaurant"	
 
 @app.route('/restaurant/<int:restaurant_id>/edit')
-def editRestaurant(restaurant_id)
+def editRestaurant(restaurant_id):
 	return "This page will be for editing restaurant %s" % restaurant_id
 
 @app.route('/restaurant/<int:restaurant_id>/delete')
-def deleteRestaurant(restaurant_id)
+def deleteRestaurant(restaurant_id):
 	return "This page will be for deleting restaurant %s" % restaurant_id
-
-@app.route('/restaurant/<int:restaurant_id>/edit')
-def editRestaurant(restaurant_id)
-	return "This page will be for editing restaurant %s" % restaurant_id
 
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
-def showMenu(restaurant_id)
+def showMenu(restaurant_id):
 	return "This page is the menu for restaurant %s" % restaurant_id
 	
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
-def newMenuItem(restaurant_id)
+def newMenuItem(restaurant_id):
 	return "This page is for making a new menu item for restaurant %s" % restaurant_id
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
-def editMenuItem(restaurant_id, menu_id)
+def editMenuItem(restaurant_id, menu_id):
 	return "This page is for editing menu item  %s" % menu_id
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
-def deleteMenuItem(restaurant_id, menu_id)
+def deleteMenuItem(restaurant_id, menu_id):
 	return "This page is for deleting menu item  %s" % menu_id	
 
 if __name__ == '__main__':
