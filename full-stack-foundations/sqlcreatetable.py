d93418f3eba5e9dc7953e197f45031944cdf21d8
Michelle Stuart <michelle.stuart@rutgers.edu>
2019-12-07 06:42:10 -0500

    changed names and reorganized folders, created jupyter notebooks

---
+++ full-stack-foundations/sqlcreatetable.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect('restaurantmenu.db')
+
+c = conn.cursor()
+c.execute('''
+		CREATE TABLE restaurant
+		(id INTEGER PRIMARY KEY ASC, name varchar(250) NOT NULL)
+		''')
+c.execute('''
+		CREATE TABLE menu_item
+		(id INTEGER PRIMARY KEY ASC, name varchar(250), price varchar(250), 
+		description varchar(250) NOT NULL, restaurant_id INTEGER NOT NULL, FOREIGH KEY(restaurant_id) REFERENCES restaurant(id))
+		''')
+
+conn.commit()
+conn.close()
+
